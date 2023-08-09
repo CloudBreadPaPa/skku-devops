9a9f345af8aa54e521bb092e3e77bb3a26ef7972
cloudbreadpapa <daewoo.kim@live.com>
2023-08-09 20:38:02 +0900

    initial commit

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-    title="FastAPI - Hello World code",
+    title="FastAPI - Hello World",
     description="This is the Hello World of FastAPI.",
     version="1.0.0",
 )
--- tests/test_main.py
+++ tests/test_main.py
@@ -11,5 +11,4 @@ def client():
 def test_hello_world(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.status_code != 500
     assert response.json() == {"Hello": "World"}
